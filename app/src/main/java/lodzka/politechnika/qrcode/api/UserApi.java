3932a852f61e4ee82dbe5b36ec3e7282cdc75760
Bartek <bartekwysocki46@gmail.com>
2018-10-10 21:47:18 +0200

    Remove annotation FormUrlEncoded

--- app/src/main/java/lodzka/politechnika/qrcode/api/UserApi.java
+++ app/src/main/java/lodzka/politechnika/qrcode/api/UserApi.java
@@ -4,11 +4,9 @@ package lodzka.politechnika.qrcode.api;
 import lodzka.politechnika.qrcode.model.User;
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 public interface UserApi {
     @POST("/registration")
-    @FormUrlEncoded
-    Call<User> registerUser(@Body User user);
+    Call<Void> registerUser(@Body User user);
 }
