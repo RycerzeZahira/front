26755362cafb41d50d953fb268a4446d3f0318f8
Bartek <bartekwysocki46@gmail.com>
2018-10-20 18:25:38 +0200

    Implement hamburger and logout

--- app/src/main/java/lodzka/politechnika/qrcode/MainActivity.java
+++ app/src/main/java/lodzka/politechnika/qrcode/MainActivity.java
@@ -3,20 +3,31 @@ package lodzka.politechnika.qrcode;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     public static final String PREFS_NAME = "userPrefs";
     public static final String LOGIN_STATUS = "loginStatus";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(lodzka.politechnika.qrcode.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+
+
         SharedPreferences preferences = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
         Boolean loginStatus = preferences.getBoolean(LOGIN_STATUS,false);
         if (loginStatus == false) {
@@ -25,13 +36,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(lodzka.politechnika.qrcode.R.menu.menu_main, menu);
-        return true;
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -42,10 +68,42 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == lodzka.politechnika.qrcode.R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.my_lists) {
+            // Handle the camera action
+        } else if (id == R.id.new_list) {
+
+        } else if (id == R.id.create_qrcode) {
+
+        } else if (id == R.id.account){
+
+        } else if (id == R.id.logout) {
+            logout();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    private void logout(){
+        SharedPreferences.Editor editor = getSharedPreferences(MainActivity.PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putBoolean(MainActivity.LOGIN_STATUS, false);
+        editor.apply();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
     }
 }
