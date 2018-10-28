aa25387eef17f5480ed9f418df29dc8976d5fb39
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2018-10-28 09:42:35 +0100

    Accept-Language header implemented

--- app/src/main/java/lodzka/politechnika/qrcode/client/RetrofitClient.java
+++ app/src/main/java/lodzka/politechnika/qrcode/client/RetrofitClient.java
@@ -1,15 +1,37 @@
 package lodzka.politechnika.qrcode.client;
 
+import java.io.IOException;
+import java.util.Locale;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit = null;
 
-    public static Retrofit getClient(String baseUrl) {
+    synchronized public static Retrofit getClient(String baseUrl) {
+
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
+            retrofit = new Retrofit.Builder().client(new OkHttpClient.Builder()
+                    .addNetworkInterceptor(new Interceptor() {
+
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request.Builder builder = chain.request().newBuilder();
+
+                            builder.addHeader("Accept-Language", Locale.getDefault().getDisplayLanguage());
+                            Request request = builder.build();
+                            Response response = chain.proceed(request);
+
+                            return response;
+                        }
+                    }).build()).baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
         }
         return retrofit;
     }
+
 }
